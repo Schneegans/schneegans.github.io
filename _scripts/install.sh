63b9dfbbc9f8ba437229e2188905f42ed99963dc
Simon Schneegans <code@simonschneegans.de>
2021-04-12 14:57:55 +0200

    Update install script

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-sudo apt-get install ruby-full build-essential zlib1g-dev
+sudo apt-get install ruby-full build-essential zlib1g-dev valac
 sudo gem install jekyll bundler
+bundle install
